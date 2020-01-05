7bfe37c76a12794d52a31c9eea7b5202a1a95453
Zed Tan <zed@shootbird.work>
2020-01-05 23:55:56 +0800

    added more complex add function test case

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -46,4 +46,54 @@ func TestNextToken(t *testing.T) {
 
 		tester(l, simpleCase)
 	})
+	t.Run("add function", func(t *testing.T) {
+		l := NewLexer(`let five = 5;
+		let ten = 10;
+		let add = fn(x, y){
+			x + y;
+		};
+		let result = add(five + ten);
+		`)
+
+		expected := []expectedTokens{
+			{token.LET, "let"},
+			{token.IDENT, "five"},
+			{token.ASSIGN, "="},
+			{token.INT, "5"},
+			{token.SEMICOLON, ";"},
+			{token.LET, "let"},
+			{token.IDENT, "ten"},
+			{token.ASSIGN, "="},
+			{token.INT, "10"},
+			{token.SEMICOLON, ";"},
+			{token.LET, "let"},
+			{token.IDENT, "add"},
+			{token.ASSIGN, "="},
+			{token.FUNCTION, "fn"},
+			{token.LPAREN, "("},
+			{token.IDENT, "x"},
+			{token.COMMA, ","},
+			{token.IDENT, "y"},
+			{token.RPAREN, ")"},
+			{token.LBRACE, "{"},
+			{token.IDENT, "x"},
+			{token.PLUS, "+"},
+			{token.IDENT, "y"},
+			{token.SEMICOLON, ";"},
+			{token.RBRACE, "}"},
+			{token.SEMICOLON, ";"},
+			{token.LET, "let"},
+			{token.IDENT, "result"},
+			{token.ASSIGN, "="},
+			{token.IDENT, "add"},
+			{token.LPAREN, "("},
+			{token.IDENT, "five"},
+			{token.PLUS, "+"},
+			{token.IDENT, "ten"},
+			{token.RPAREN, ")"},
+			{token.SEMICOLON, ";"},
+			{token.EOF, ""},
+		}
+		tester(l, expected)
+	})
 }
