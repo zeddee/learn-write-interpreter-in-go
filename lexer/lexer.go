11427df533b09b6d3d41ad686dfe376135cb78a5
Zed Tan <zed@shootbird.work>
2020-01-06 09:23:45 +0800

    added skipWhitespace() so that we ignore white space that is not EOF; added ability to read identifiers

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -11,7 +11,7 @@ type Lexer struct {
 }
 
 // New initialises a new lexer
-func New(input string) *Lexer {
+func NewLexer(input string) *Lexer {
 	l := &Lexer{input: input}
 	l.readChar()
 	return l
@@ -20,6 +20,8 @@ func New(input string) *Lexer {
 // NextToken returns the next token in the input string
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+
+	l.skipWhitespace()
 
 	switch l.ch {
 	case '=':
@@ -41,6 +43,21 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		// add default case to catch cases
+		// where we need to lex a sequence of chars
+		// e.g. for any identifiers
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			// we're exiting early here because
+			// we've already called l.readChar()
+			// (which we _must_ call in order to get
+			// the complete identifer string)
+			// inside our readIdentifier() call.
+			return tok
+		}
+		tok = newToken(token.ILLEGAL, l.ch)
 	}
 
 	l.readChar()
@@ -62,4 +79,37 @@ func (l *Lexer) readChar() {
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+// readIdentifier reads in a continuous sequence of
+// characters and stops when isLetter() returns false
+func (l *Lexer) readIdentifier() string {
+	// store the current value of l.position
+	position := l.position
+	for isLetter(l.ch) {
+		// read characters in 'input',
+		// check if character fulfils isLetter()
+		// and advance l.position
+		// once we hit isLetter() == false,
+		// we stop and return the string
+		// of characters that we've read
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// isLetter checks if ch is a letter.
+// the scope of this checker will determine
+// what characters will be legal in keywords
+// and identifiers.
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+// skipWhitespace skips whitespace that does
+// not qualify as EOF
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
 }
--- token/token.go
+++ token/token.go
@@ -36,3 +36,23 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+// LookupIdent  takes in string ident
+// and checks if we have the value of
+// ident stored in our 'keywords' map
+// if it exists in our 'keywords' map,
+// then we look up the keyword and
+// return the stored TokenType.
+// Otherwise, we'll treat it as an identifier
+// and return a TokenType of IDENT.
+func LookupIdent(ident string) TokenType {
+	if tokenType, ok := keywords[ident]; ok {
+		return tokenType
+	}
+	return IDENT
+}
